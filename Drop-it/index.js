b64765066937bccf8b2fbc88d81523f768dda5af
Jc6300 <joshcotton93@gmail.com>
2023-05-04 22:48:19 -0500

    daily dsa

---
+++ Drop-it/index.js
@@ -0,0 +1,14 @@
+function dropElements(arr, func) {
+	for (let i = 0; i < arr.length; i++) {
+		if (func(arr[i])) {
+			return arr.slice(i);
+		}
+	}
+	return [];
+}
+
+console.log(
+	dropElements([1, 2, 3], function (n) {
+		return n < 3;
+	})
+); // output: [1, 2]
