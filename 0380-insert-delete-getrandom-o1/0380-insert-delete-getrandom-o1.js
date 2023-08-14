56f7aa6841870d4bcb8f5a41cd6f7904c103140a
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-08-14 14:15:06 -0500

    Time: 304 ms (97.75%), Space: 114 MB (38.73%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -0,0 +1,42 @@
+ class RandomizedSet{
+     constructor() {
+        this.map = new Map()
+        this.arr = []
+     }
+
+     insert(val){
+         if(this.map.has(val)){
+             return false
+         }
+         this.map.set(val, this.arr.length)
+         this.arr.push(val)
+         return true
+     }
+
+     remove(val){
+         if (!this.map.has(val)){
+             return false
+         }
+         
+    let index = this.map.get(val)
+    let lastElement = this.arr[this.arr.length -1]
+    this.arr[index] = lastElement
+    this.map.set(lastElement, index)
+    this.arr.pop()
+    this.map.delete(val)
+    return true
+     }
+
+     getRandom(){
+         let randomIndex = Math.floor(Math.random() * this.arr.length)
+         return this.arr[randomIndex]
+     }
+ }
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
