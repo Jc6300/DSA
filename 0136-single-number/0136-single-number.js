a489274ceed1494a5a702ed9b6f9f5103eca8faa
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-08-18 20:35:11 -0500

    Time: 54 ms (91.08%), Space: 44.2 MB (65.66%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNumber(nums){
+    let result = 0
+    
+    for(const num of nums){
+        result ^= num
+    }
+    return result
+}
