efc838932aec012fd57f6e1d9cfaf6d858a07896
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-09-03 18:36:50 -0500

    Time: 62 ms (62.51%), Space: 44.4 MB (24.33%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -9,25 +9,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
+
 
 const deleteDuplicates = (head) => {
-  let current = head;
-
-  while (current && current.next) {
-    if (current.val === current.next.val) {
-  
-      current.next = current.next.next;
-    } else {
-     
-      current = current.next;
+let current = head;
+    
+    while(current) {
+        if(current.next !== null && current.val == current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
     }
-  }
-
-  return head;
+    
+    return head;
 };
+
