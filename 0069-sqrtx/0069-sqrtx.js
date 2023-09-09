d52277694a202a42bf127250f9eac5bdd99d0d53
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-09-08 23:36:16 -0500

    Time: 59 ms (80.97%), Space: 43.8 MB (40.9%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,18 +2,31 @@
  * @param {number} x
  * @return {number}
  */
-const mySqrt = (x) => {
-    if ( x === 0 || x ===1){
-        return x
-    }
+// const mySqrt = (x) => {
+//     if ( x === 0 || x === 1){
+//         return x
+//     }
 
-    let guess = x
-    while(true){
-        const newGuess = (guess + x / guess) / 2
+//     let guess = x
+//     while(true){
+//         const newGuess = (guess + x / guess) / 2
 
-        if(Math.abs(newGuess - guess) < 1e-6){
-            return Math.floor(newGuess)
-        }
-        guess = newGuess
+//         if(Math.abs(newGuess - guess) < 1e-6){
+//             return Math.floor(newGuess)
+//         }
+//         guess = newGuess
+//     }
+// };
+
+
+function mySqrt(x) {
+    if (x < 2) return x;
+    let left = 1, right = Math.floor(x / 2);
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (mid * mid === x) return mid;
+        else if (mid * mid < x) left = mid + 1;
+        else right = mid - 1;
     }
-};
+    return right;
+}
