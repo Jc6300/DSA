7641699e9ff50a925b75caece928df3a65adbddc
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-09-15 13:42:47 -0500

    Time: 66 ms (13.53%), Space: 42.6 MB (25.55%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = (candies, extraCandies) => {
+ const maxCandies = Math.max(...candies)
+
+ const result = []
+
+ for(let i = 0; i < candies.length; i++){
+   result.push(candies[i] + extraCandies >= maxCandies)
+ }
+ return result
+};
