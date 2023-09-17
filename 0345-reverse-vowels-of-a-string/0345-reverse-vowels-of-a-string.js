38386c89fe40439329bde6c1e141ef1116efaaf7
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-09-17 13:51:01 -0500

    Time: 70 ms (63.87%), Space: 47.5 MB (88.05%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,8 +3,35 @@
  * @return {string}
  */
 function reverseVowels(s) {
-    // Get an array of all vowels
-    const vowels = s.match(/[aeiou]/gi)
-    // Replace all vowels with the last element in `vowels` array
-    return s.replace(/[aeiou]/gi, () => vowels.pop())
-};
+  // Convert the string to an array of characters for easier manipulation.
+  const chars = s.split('');
+  
+  // Define a set of vowels to check against (both lower and upper case).
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  
+  // Initialize two pointers.
+  let left = 0;
+  let right = chars.length - 1;
+  
+  while (left < right) {
+    // Find the next vowel from the left side.
+    while (left < right && !vowels.has(chars[left])) {
+      left++;
+    }
+    
+    // Find the next vowel from the right side.
+    while (left < right && !vowels.has(chars[right])) {
+      right--;
+    }
+    
+    // Swap the vowels.
+    [chars[left], chars[right]] = [chars[right], chars[left]];
+    
+    // Move the pointers towards the center.
+    left++;
+    right--;
+  }
+  
+  // Convert the array back to a string and return it.
+  return chars.join('');
+}
