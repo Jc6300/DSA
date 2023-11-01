184c5b09c024d10058a38fe0da6966c5566e3f3c
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-11-01 12:29:23 -0500

    Time: 56 ms (33.29%), Space: 42.1 MB (37.24%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -7,9 +7,10 @@
 */
 
 
- function merge(nums1, m, nums2, n) {
-    for (let i = m, j = 0; j < n; i++, j++) {
-        nums1[i] = nums2[j];
+
+const merge = (nums1,m,nums2,n) =>{
+    for(let i = m, j = 0; j < n; i++, j++){
+        nums1[i] = nums2[j]
     }
-    nums1.sort((a, b) => a - b);
-};
+    nums1.sort((a,b)=> a - b)
+}
