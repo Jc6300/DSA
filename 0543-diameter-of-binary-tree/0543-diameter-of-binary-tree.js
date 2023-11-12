2a751b0ba0a777642ed8293b245fa4af8478a5c8
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-11-12 12:21:14 -0600

    Time: 54 ms (87.73%), Space: 44.7 MB (98.41%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const diameterOfBinaryTree = function (root) {
+  let diameter = 0;
+
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    // Recursively calculate the depth of the left and right subtrees
+    const leftDepth = dfs(node.left);
+    const rightDepth = dfs(node.right);
+
+    // Update the diameter at each node
+    diameter = Math.max(diameter, leftDepth + rightDepth);
+
+    // Return the depth of the subtree rooted at this node
+    return 1 + Math.max(leftDepth, rightDepth);
+  };
+
+  dfs(root);
+
+  return diameter;
+};
+
+
