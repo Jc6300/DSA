c52129e32511919a8ba25d6dd2310637bafad977
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2023-11-12 12:22:31 -0600

    Time: 61 ms (59.26%), Space: 45.8 MB (51.8%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -24,14 +24,11 @@ const diameterOfBinaryTree = function (root) {
   const dfs = (node) => {
     if (!node) return 0;
 
-    // Recursively calculate the depth of the left and right subtrees
     const leftDepth = dfs(node.left);
     const rightDepth = dfs(node.right);
 
-    // Update the diameter at each node
     diameter = Math.max(diameter, leftDepth + rightDepth);
 
-    // Return the depth of the subtree rooted at this node
     return 1 + Math.max(leftDepth, rightDepth);
   };
 
