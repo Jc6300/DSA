06ae66bf413bdb735ddf9923815c54cfc1eab36c
Jc6300 <joshcotton93@gmail.com>
2023-11-30 21:03:57 -0600

    daily dsa

---
+++ Generate-Fibonacci-Sequence/index.js
@@ -0,0 +1,24 @@
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+/**
+ * @return {Generator<number>}
+ */
+const fibGenerator = function* () {
+	let a = 0,
+		b = 1;
+
+	while (true) {
+		yield a;
+		[a, b] = [b, a + b];
+	}
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
