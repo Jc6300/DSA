b6f3733d280fd3fb5f8451c96af5e63535301e81
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2024-02-14 19:37:47 -0600

    Time: 340 ms (20.74%) | Memory: 99.3 MB (24.02%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+let rearrangeArray =  (nums)=> {
+    let positiveNums = nums.filter(x => x > 0)
+    let negativeNums = nums.filter(x => x < 0)
+    let result = []
+
+    for (let i = 0; i < nums.length / 2; i++) {
+        result.push(positiveNums[i], negativeNums[i])
+    }
+
+    return result
+};
