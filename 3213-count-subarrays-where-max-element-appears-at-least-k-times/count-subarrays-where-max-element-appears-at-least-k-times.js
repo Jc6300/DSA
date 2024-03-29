9a626232d3920171a76fc5d297868f64a9b34278
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2024-03-29 18:35:28 -0500

    Time: 107 ms (32.14%) | Memory: 62.7 MB (39.29%) - LeetSync

---
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+
+
+const countSubarrays =(nums, k)=> {
+    let mx = Math.max(...nums);
+    let ans = 0;
+    let l = 0;
+    let r = 0;
+    const n = nums.length;
+
+    while (r < n) {
+        k -= nums[r] === mx ? 1 : 0;
+        r++;
+        while (k === 0) {
+            k += nums[l] === mx ? 1 : 0;
+            l++;
+        }
+        ans += l;
+    }
+
+    return ans;
+};
