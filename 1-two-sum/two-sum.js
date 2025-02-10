966eb54754b78cc98dcb3c61d1b13173db85fe55
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2025-02-10 17:25:35 -0600

    Time: 1 ms (90.28%) | Memory: 52.2 MB (10.10%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,35 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
+var twoSum = function(nums, target) {
+    const prevMap = new Map()
 
-    const numIndices = new Map();
-    
-    for (let i = 0; i < nums.length; i++) {
-
-        const complement = target - nums[i];
-        
-        if (numIndices.has(complement)) {
-        
-            return [numIndices.get(complement), i];
+    for(let i = 0; i < nums.length; i++){
+        const diff = target - nums[i]
+        if(prevMap.has(diff)){
+            return [prevMap.get(diff), i]
         }
-        
-        numIndices.set(nums[i], i);
+        prevMap.set(nums[i],i)
     }
-    
-
-    return [];
-}
-
-// Example usage:
-const nums1 = [2, 7, 11, 15];
-const target1 = 9;
-console.log(twoSum(nums1, target1));  // Output: [0, 1]
-
-const nums2 = [3, 2, 4];
-const target2 = 6;
-console.log(twoSum(nums2, target2));  // Output: [1, 2]
-
-const nums3 = [3, 3];
-const target3 = 6;
-console.log(twoSum(nums3, target3));  // Output: [0, 1]
+    return []
+};
