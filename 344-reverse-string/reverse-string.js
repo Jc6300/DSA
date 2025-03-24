147dee572803ecab43a195a7fcce18cb8e00a17d
Jc6300 <65563394+Jc6300@users.noreply.github.com>
2025-03-24 13:01:35 -0500

    Time: 1 ms (53.99%) | Memory: 62.5 MB (8.56%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,14 +3,9 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-//  
-let left = 0
-let right = s.length - 1;
-while(left < right){
-    [s[left], s[right]] = [s[right], s[left]]
-
-    left++
-    right--
-}
- 
+    const n = Math.floor(s.length / 2);
+    for (let i = 0; i < n; i++) {
+   
+        [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+    }
 };
