8a3c87f22d710851173f63d4a03882bf0e0768de
JCTheDev <65563394+Jc6300@users.noreply.github.com>
2025-06-11 17:41:21 -0500

    Time: 2 ms (74.22%) | Memory: 55.8 MB (87.58%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+      let res = 0
+        let count =0
+        for(let num of nums){
+            if(count === 0){
+                res = num
+            }
+            count += (num === res) ? 1: -1
+        }
+        return res
+};
