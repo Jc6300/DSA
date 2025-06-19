e5bb31ab57d005f11b2be542c308ea8a53ee9230
JCTheDev <65563394+Jc6300@users.noreply.github.com>
2025-06-18 23:55:56 -0500

    Time: 26 ms (12.03%) | Memory: 67.1 MB (5.21%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -3,28 +3,24 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    const freqMap = new Map();
-    
+ const topKFrequent = function(nums, k) {
+        const count = {};
+        const freq = Array.from({ length: nums.length + 1 }, () => []);
 
-    for (let num of nums) {
-        freqMap.set(num, (freqMap.get(num) || 0) + 1);
-    }
-    
+        for (const n of nums) {
+            count[n] = (count[n] || 0) + 1;
+        }
+        for (const n in count) {
+            freq[count[n]].push(parseInt(n));
+        }
 
-    const buckets = Array(nums.length + 1).fill().map(() => []);
-    for (let [num, freq] of freqMap.entries()) {
-        buckets[freq].push(num);
-    }
-
-
-    const result = [];
-    for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
-        for (let num of buckets[i]) {
-            result.push(num);
-            if (result.length === k) break;
+        const res = [];
+        for (let i = freq.length - 1; i > 0; i--) {
+            for (const n of freq[i]) {
+                res.push(n);
+                if (res.length === k) {
+                    return res;
+                }
+            }
         }
     }
-
-    return result;
-};
