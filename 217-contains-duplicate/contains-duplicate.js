8efa3095663e8b95026524404dc8f58c7bdb3e96
JCTheDev <65563394+Jc6300@users.noreply.github.com>
2025-10-21 14:57:37 -0500

    Time: 15 ms (84.16%) | Memory: 73.8 MB (52.00%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -6,10 +6,10 @@ var containsDuplicate = function(nums) {
  // Create a Set to store unique numbers
     const seen = new Set()
 //loop over each num in array
-    for(let num of nums){
+    for (let num of nums){
         //If the number is already in the Set, we found a duplicate
         if (seen.has(num)) return true
-        // if haven't seen the number, add it to the set
+        // if we haven't seen the number, add it to the set
         seen.add(num)
     }
     // f no duplicates were found, return false
